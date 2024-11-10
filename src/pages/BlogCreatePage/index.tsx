9002a6c04214ff26367f73a5699653cbdbd31d05
ivory-code <kyo3553@gmail.com>
2024-11-10 14:23:04 +0900

    fix: fix build issue

--- src/pages/BlogCreatePage/index.tsx
+++ src/pages/BlogCreatePage/index.tsx
@@ -5,6 +5,8 @@ import React from 'react'
 import BlogEdit from '@/features/blog/components/BlogEdit'
 import Layout from '@/widgets/Layout/components'
 
+export const runtime = 'edge'
+
 const BlogCreatePage = () => {
   return (
     <div>
